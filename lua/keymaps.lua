d9cf591d850d6fa71f7c46247ad369e7f05eec98
OlegDeb <stweb.ru@ya.ru>
2026-09-04 19:25:28 +0300

    add keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,3 +7,27 @@ map("n", "<leader>e", ":Neotree toggle<CR>", {
 	noremap = true,
 	silent = true,
 })
+
+-- Выход из режима вставки через jj
+vim.keymap.set("i", "jj", "<Esc>", {
+	noremap = true,
+	silent = true,
+})
+
+-- Быстрое сохранение
+vim.keymap.set("n", "<leader>w", ":w<CR>", {
+	noremap = true,
+	silent = true,
+})
+
+-- Быстрое закрытие
+vim.keymap.set("n", "<leader>q", ":q<CR>", {
+	noremap = true,
+	silent = true,
+})
+
+-- Сохранить всё и выйти
+vim.keymap.set("n", "<leader>x", ":xa<CR>", {
+	noremap = true,
+	silent = true,
+})
